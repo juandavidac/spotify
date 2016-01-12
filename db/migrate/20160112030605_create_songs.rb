9c05d01c40cf1c875fb896a2c3440e21574f7e07
juandavidac <jd.ac@outlook.com>
2016-01-11 22:16:30 -0500

    crea modelos

---
+++ db/migrate/20160112030605_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :number
+      t.string :preview_url
+      t.references :album, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :songs, :albums
+  end
+end
